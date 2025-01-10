cb9674c6637f06e1afa8e4d237e8a7a9446da311
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2025-01-09 23:14:01 -0500

    Initial file setup and model for user

---
+++ models/user.ts
@@ -0,0 +1,53 @@
+import {Schema,model,type Document} from 'mongoose';
+
+interface Iuser extends Document{
+    username:string,
+    email:string,
+    thoughts:Schema.Types.ObjectId[],
+    friends:Schema.Types.ObjectId[],
+}
+
+const userSchema = new Schema<Iuser>(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            trim:true,
+        },
+        email:{
+        type:String,
+        required:true,
+        unique:true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please enter a valid email address'],
+        // match:[/.+@.+\..+/],   // need to check this  
+        },
+        thoughts:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:'Thought'
+            },
+        ],
+        friends:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:'User'
+            },
+        ],
+    },
+    {
+        toJSON:{
+            virtuals:true,
+        } 
+    }    
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model<Iuser>('User',userSchema);
+
+export default User;
+
+
